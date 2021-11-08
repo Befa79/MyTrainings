f572353ffaebf711e18c8a76775cec7bafd35b79
Befa79 <robindubois79@hotmail.com>
2021-11-08 17:04:09 +0000

    msg

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var sidenavs = document.querySelectorAll('.sidenav');
+    var sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+  });
